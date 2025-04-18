7a5924b1c7e0bf90d027f9ef84c8d4c68311d0b5
Ravina106 <ranagaravina@gmail.com>
2025-04-18 16:48:19 +0530

    Fixed import path in env file

--- api/index.js
+++ api/index.js
@@ -14,7 +14,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+      'http://localhost:3000',  
+      'https://crud-server-tan.vercel.app'
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  credentials: true
+}));
 
 // Routes
 app.use("/api", route);
